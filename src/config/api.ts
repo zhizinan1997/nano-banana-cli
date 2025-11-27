bdffa45e801122c4db5486f0e7973b3272ff6f10
Ryan Zhi <137845868+zhizinan1997@users.noreply.github.com>
2025-11-27 10:49:59 +0800

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,3 +1,3 @@
 export const DEFAULT_API_ENDPOINT = 'https://newapi2.zhizinan.top/v1/chat/completions'
 export const DEFAULT_MODEL_ID = 'gemini-draw'
-export const DEFAULT_API_KEY = 'sk-w4eyFPGlmIRm1gZBZeyfXKGX6dIL7iy2LAe0j6fw4Ws3aXDM'
+export const DEFAULT_API_KEY = 'sk-P2JJawbc9W1r7NmA0Reqby9ZoQoFJtrEssE19Zzrm8lnVn2z'
